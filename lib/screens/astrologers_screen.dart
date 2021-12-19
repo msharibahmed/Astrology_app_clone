33106c7f2f550394771337320a4280a37c5ac077
Sharib <msharibahmed@gmail.com>
2021-12-19 19:21:45 +0530

    BUILD: BottomNavigationBar

---
+++ lib/screens/astrologers_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class AstrologersScreen extends StatelessWidget 
+{
+  const AstrologersScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Astrologer'),
+      
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+import 'astrologers_screen.dart';
+import 'panchang_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  List<StatelessWidget> screens = const [PanchangScreen(), AstrologersScreen()];
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('hello'),),
-      
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.chat_bubble_outline_sharp),
+                label: 'Talk to Astrologer'),
+          ]),
+      body: screens[_currentIndex],
     );
   }
 }
