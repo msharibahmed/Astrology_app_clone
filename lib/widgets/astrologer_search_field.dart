430a69446476028383406032cc00e9d7625fbb3d
Sharib <msharibahmed@gmail.com>
2021-12-23 19:19:44 +0530

    CHANGE:  Astrologer search textfield styling

--- lib/widgets/astrologer_search_field.dart
+++ lib/widgets/astrologer_search_field.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:india_today_assignment/providers/astrolger_provider.dart';
 import 'package:provider/provider.dart';
@@ -24,31 +25,55 @@ class _AstrologerSearchFieldState extends State<AstrologerSearchField> {
         key: _formStateKey,
         child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 50.0),
-            child: TextFormField(
-                controller: _astroSeacrhCtrl,
-                decoration: InputDecoration(
-                    icon: const Icon(Icons.keyboard),
-                    hintText: 'Search Astrolger, name, skills, experience etc.',
-                    suffixIcon: IconButton(
-                        onPressed: () {
-                          final _prov = Provider.of<AstrologerProv>(context,
-                              listen: false);
-
-                          if (_astroSeacrhCtrl.text.trim() != '') {
-                            _prov.clearTextField();
-                          } else {
-                            _prov.toggleShowSearchBar();
-                          }
-                          _astroSeacrhCtrl.clear();
-                        },
-                        icon: const Icon(
-                          Icons.clear_rounded,
-                          size: 18,
-                        ))),
-                onChanged: (input) {
-                  Provider.of<AstrologerProv>(context, listen: false)
-                      .showAstrolgersBySearch(input);
-                  // print(input);
-                })));
+            child: Material(
+              elevation: 2,
+              child: CupertinoTextField(
+                  controller: _astroSeacrhCtrl,
+                  placeholder: 'Search Astrolger, name, skills, experience etc.',
+                  prefix: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.keyboard),
+                  ),
+                  suffix: IconButton(
+                      onPressed: () {
+                        final _prov =
+                            Provider.of<AstrologerProv>(context, listen: false);
+            
+                        if (_astroSeacrhCtrl.text.trim() != '') {
+                          _prov.clearTextField();
+                        } else {
+                          _prov.toggleShowSearchBar();
+                        }
+                        _astroSeacrhCtrl.clear();
+                      },
+                      icon: const Icon(
+                        Icons.clear_rounded,
+                        size: 18,
+                      )),
+                  // decoration: InputDecoration(
+                  //     icon: const Icon(Icons.keyboard),
+                  //     hintText: 'Search Astrolger, name, skills, experience etc.',
+                  //     suffixIcon: IconButton(
+                  //         onPressed: () {
+                  //           final _prov = Provider.of<AstrologerProv>(context,
+                  //               listen: false);
+            
+                  //           if (_astroSeacrhCtrl.text.trim() != '') {
+                  //             _prov.clearTextField();
+                  //           } else {
+                  //             _prov.toggleShowSearchBar();
+                  //           }
+                  //           _astroSeacrhCtrl.clear();
+                  //         },
+                  //         icon: const Icon(
+                  //           Icons.clear_rounded,
+                  //           size: 18,
+                  //         ))),
+                  onChanged: (input) {
+                    Provider.of<AstrologerProv>(context, listen: false)
+                        .showAstrolgersBySearch(input);
+                    // print(input);
+                  }),
+            )));
   }
 }
