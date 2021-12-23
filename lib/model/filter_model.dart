0cb5d68fa63c7b3a683823cee666f940047dc0c1
Sharib <msharibahmed@gmail.com>
2021-12-23 23:55:45 +0530

    ADD: Filter Astrolgers logic

---
+++ lib/model/filter_model.dart
@@ -0,0 +1,5 @@
+class FilterChipsModel {
+  String name;
+  bool isSelected;
+  FilterChipsModel({required this.isSelected, required this.name});
+}
--- lib/providers/astrolger_provider.dart
+++ lib/providers/astrolger_provider.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:india_today_assignment/model/astrologer_model.dart';
+import 'package:india_today_assignment/model/filter_model.dart';
 import 'package:india_today_assignment/utils/constants.dart';
 
 class AstrologerProv with ChangeNotifier {
@@ -19,6 +20,7 @@ class AstrologerProv with ChangeNotifier {
   SortNames _sortNamesTag = SortNames.All;
   SortNames get sortNamesTag => _sortNamesTag;
   void sortBySelection(SortNames selectedName) {
+    // clearFilters(null, false);
     _sortNamesTag = selectedName;
     switch (selectedName) {
       case SortNames.Increasing_Exp:
@@ -45,37 +47,39 @@ class AstrologerProv with ChangeNotifier {
 
   void _sortbyExpIncreasing() {
     List<AstrologerModel> _tempList = [..._astrologersList];
-    _tempList.sort((a, b) => a.experience.compareTo(b.experience));
+    _searchAstrolgerList.sort((a, b) => a.experience.compareTo(b.experience));
 
-    _searchAstrolgerList = _tempList;
+    // _searchAstrolgerList = _tempList;
 
     notifyListeners();
   }
 
   void _sortbyExpDecreasing() {
     List<AstrologerModel> _tempList = [..._astrologersList];
-    _tempList.sort((a, b) => b.experience.compareTo(a.experience));
-    _searchAstrolgerList = _tempList;
+    _searchAstrolgerList.sort((a, b) => b.experience.compareTo(a.experience));
+    // _searchAstrolgerList = _tempList;
     notifyListeners();
   }
 
   void _sortbyPriceIncreasing() {
     List<AstrologerModel> _tempList = [..._astrologersList];
-    _tempList.sort((a, b) =>
+    _searchAstrolgerList.sort((a, b) =>
         a.additionalPerMinuteCharges.compareTo(b.additionalPerMinuteCharges));
-    _searchAstrolgerList = _tempList;
+    // _searchAstrolgerList = _tempList;
     notifyListeners();
   }
 
   void _sortbyPriceDecreasing() {
     List<AstrologerModel> _tempList = [..._astrologersList];
-    _tempList.sort((a, b) =>
+    _searchAstrolgerList.sort((a, b) =>
         b.additionalPerMinuteCharges.compareTo(a.additionalPerMinuteCharges));
-    _searchAstrolgerList = _tempList;
+    // _searchAstrolgerList = _tempList;
     notifyListeners();
   }
 
   void _clearSorting() {
+    clearFilters(null, false);
+    
     List<AstrologerModel> _tempList = _astrologersList;
     _searchAstrolgerList = _tempList;
     notifyListeners();
@@ -93,7 +97,7 @@ class AstrologerProv with ChangeNotifier {
       final http.Response _response = await http.get(_url);
       final _decodedResponse = jsonDecode(_response.body);
 
-      print(_decodedResponse);
+      // print(_decodedResponse);
 
       if (_decodedResponse['httpStatusCode'] == 200) {
         List<AstrologerModel> _tempAstrosList = [];
@@ -109,7 +113,7 @@ class AstrologerProv with ChangeNotifier {
       debugPrint('No net');
       throw Exception('No Internet connection');
     } catch (error) {
-      debugPrint(error.toString() + 'helllllllo');
+      debugPrint(error.toString() + ' error in getAllAstrolgers function');
     }
 
     notifyListeners();
@@ -127,6 +131,8 @@ class AstrologerProv with ChangeNotifier {
   }
 
   void showAstrolgersBySearch(String input) {
+    clearFilters(null, false);
+
     if (input.trim() != '') {
       input = input.trim();
       _searchAstrolgerList = _astrologersList.where((e) {
@@ -156,5 +162,81 @@ class AstrologerProv with ChangeNotifier {
 
   /////
   ///
+  ///
+  final List<FilterChipsModel> _skillName = [
+    FilterChipsModel(isSelected: false, name: 'Coffe Cup Reading'),
+    FilterChipsModel(isSelected: false, name: 'Falit Jyotish'),
+    FilterChipsModel(isSelected: false, name: 'Kundali Grah Dosh'),
+    FilterChipsModel(isSelected: false, name: 'Vastu'),
+    FilterChipsModel(isSelected: false, name: 'Vedic Astrology'),
+    FilterChipsModel(isSelected: false, name: 'Astrology'),
+    FilterChipsModel(isSelected: false, name: 'Palmistry'),
+    FilterChipsModel(isSelected: false, name: 'Numerology'),
+    FilterChipsModel(isSelected: false, name: 'Face Reading'),
+    FilterChipsModel(isSelected: false, name: 'Tarot'),
+  ];
 
+  final List<FilterChipsModel> _languageName = [
+    FilterChipsModel(isSelected: false, name: 'English'),
+    FilterChipsModel(isSelected: false, name: 'Hindi'),
+  ];
+
+  List<FilterChipsModel> get skillName => [..._skillName];
+  List<FilterChipsModel> get languageName => [..._languageName];
+  void skillSelected(String skill) {
+    int index = _skillName.indexWhere((element) => element.name == skill);
+    _skillName[index].isSelected = !(_skillName[index].isSelected);
+    notifyListeners();
+  }
+
+  void languageSelected(String language) {
+    int index = _languageName.indexWhere((element) => element.name == language);
+    _languageName[index].isSelected = !(_languageName[index].isSelected);
+    notifyListeners();
+  }
+
+  void applyFilters(BuildContext ctx) {
+    List<FilterChipsModel> _selectedSkills = [];
+    List<FilterChipsModel> _selectedLanguages = [];
+
+    _selectedSkills =
+        _skillName.where((skill) => skill.isSelected == true).toList();
+    _selectedLanguages =
+        _languageName.where((language) => language.isSelected == true).toList();
+
+    _searchAstrolgerList = _searchAstrolgerList.where((astro) {
+      String _astroSkills = astro.skills.map((e) => e.name).toList().join('');
+      String _astrolanguages =
+          astro.languages.map((e) => e.name).toList().join('');
+
+      bool _skillBool = _selectedSkills
+          .every((element) => _astroSkills.contains(element.name));
+      bool _langBool = _selectedLanguages
+          .every((element) => _astrolanguages.contains(element.name));
+
+      if (_skillBool && _langBool) {
+        return true;
+      } else {
+        return false;
+      }
+    }).toList();
+
+    notifyListeners();
+    Navigator.pop(ctx);
+  }
+
+  void clearFilters(BuildContext? ctx, [bool pop = true]) {
+    _searchAstrolgerList = _astrologersList;
+    for (var x in _skillName) {
+      x.isSelected = false;
+    }
+    for (var y in _languageName) {
+      y.isSelected = false;
+    }
+
+    notifyListeners();
+    if (pop) {
+      Navigator.pop(ctx!);
+    }
+  }
 }
